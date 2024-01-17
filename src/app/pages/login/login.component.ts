cf49eee244a5fd63a2b0a895c623ebf615958583
JarYin <tempest353@hotmail.com>
2024-01-17 20:31:58 +0700

    .

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,56 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private route: Router) {}
 
+  change = false;
+  changeButton(value: boolean) {
+    this.change = value;
+  }
+  ngOnit() {
+    const localData = localStorage.getItem('signupUsers');
+    if (localData != null) {
+      this.signupUsers = JSON.parse(localData);
+    }
+  }
+
+  signupUsers: any[] = [];
+  signupObj: any = {
+    username: '',
+    email: '',
+    password: '',
+  };
+
+  loginObj: any = {
+    email: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  };
+
+  onSignup() {
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+  }
+
+  loginForm() {
+    const isUserExist = this.signupUsers.find(
+      (m) =>
+        m.email == this.loginObj.email && m.password == this.loginObj.password
+    );
+    if (isUserExist != undefined) {
+      alert('Login Successful');
+      this.route.navigate(['/']);
+    } else {
+      alert('Login Failed');
+    }
+  }
 }
